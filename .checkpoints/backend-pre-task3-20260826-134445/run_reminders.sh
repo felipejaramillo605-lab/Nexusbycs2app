4cee1f7e5997cc1d3d8b599e1ff8e9e9eba423d9
Felipe Jaramillo <felipejaramillo605@gmail.com>
2026-08-26 14:01:12 +0000

    chore: stop tracking .checkpoints (local backup snapshots)

--- .checkpoints/backend-pre-task3-20260826-134445/run_reminders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Cron job to run reminder service every hour
-# Add to crontab with: crontab -e
-# Then add this line:
-# 0 * * * * /app/backend/run_reminders.sh >> /var/log/nexus_reminders.log 2>&1
-
-cd /app/backend
-python3 send_reminders.py
